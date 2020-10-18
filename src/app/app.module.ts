64ff7416278bb9cf7faddc85508e869bcea3a6d2
saykondesu <isaaclagface@gmail.com>
2020-10-17 23:32:36 -0300

    oi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CadastroPipe } from './cadastro.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CadastroPipe],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
